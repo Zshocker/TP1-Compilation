84cc766ba99aa545b3056ed27735de12a45d7341
Zshocker <hicham.sentel-etu@etu.univh2c.ma>
2021-10-21 13:00:41 -0700

    save

--- structs.h
+++ structs.h
@@ -9,3 +9,8 @@ typedef enum token_type{
     begin,end,read,write,id,intliteral,rparem,lparem,semicolon,comma,assignOp,plusOp,minusOp,floatt,scanof,err
 }token;
 char TableTokens[16][20]={"begin","end","read","write","id","intliteral","rparem","lparem","semicolon","comma","assignOp","plusOp","minusOp","float","scanof"};
+typedef enum {
+    begin,end,read,write,id,intliteral,rparem,lparem,semicolon,comma,assignOp,plusOp,minusOp,floatt,scanof,system_global,program,inst_list,inst,id_list,expression,expr_list,add_op,prim,err
+}Axiom;
+char TableAxiom[32][20]={"begin","end","read","write","id","intliteral","rparem","lparem","semicolon","comma","assignOp","plusOp","minusOp","float","scanof",
+                        "system_global","program","inst_list","expression","expr_list","add_op","prim"};
